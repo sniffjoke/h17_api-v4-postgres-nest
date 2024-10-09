bf6c4da29fdecb7a381455d841bc167874af46fe
sniffjoke <sniffjoke@gmail.com>
2024-10-09 22:49:54 +0200

    add basic-guards controller and service for users

--- src/features/users/api/users.controller.ts
+++ src/features/users/api/users.controller.ts
@@ -1,17 +1,20 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from '../application/users.service';
 import { CreateUserDto } from './models/input/create-user.dto';
+import { BasicAuthGuard } from '../../../core/guards/basic-auth.guard';
 
 @Controller('sa')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('users')
+  @UseGuards(BasicAuthGuard)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get('users')
+  @UseGuards(BasicAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
@@ -23,6 +26,7 @@ export class UsersController {
 
 
   @Delete('users/:id')
+  @UseGuards(BasicAuthGuard)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -17,25 +17,25 @@ async function bootstrap() {
   }))
   app.useGlobalFilters(new BadRequestExceptionFilter())
   useContainer(app.select(AppModule), {fallbackOnErrors: true})
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      stopAtFirstError: true,
-      exceptionFactory: (errors) => {
-        const customErrors = [];
-        errors.forEach((e) => {
-          const constraintKeys = Object.keys(e.constraints as any);
-          constraintKeys.forEach((cKey, index) => {
-            if (index >= 1) return;
-            const msg = e.constraints?.[cKey] as any;
-            // @ts-ignore
-            customErrors.push({ field: e.property, message: msg });
-          });
-        });
-        throw new BadRequestException(customErrors);
-      },
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     stopAtFirstError: true,
+  //     exceptionFactory: (errors) => {
+  //       const customErrors = [];
+  //       errors.forEach((e) => {
+  //         const constraintKeys = Object.keys(e.constraints as any);
+  //         constraintKeys.forEach((cKey, index) => {
+  //           if (index >= 1) return;
+  //           const msg = e.constraints?.[cKey] as any;
+  //           // @ts-ignore
+  //           customErrors.push({ field: e.property, message: msg });
+  //         });
+  //       });
+  //       throw new BadRequestException(customErrors);
+  //     },
+  //   }),
+  // );
   app.use(cookieParser())
   await app.listen(SETTINGS.PORT, () => console.log('DB connect'));
 }
